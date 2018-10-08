22782d9a8c61dfca7923ed484d64b28fca0c2575
dqhuy78 <dqhuy78@gmail.com>
2018-10-08 11:37:56 +0700

    Update support name

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -14,6 +14,7 @@ class WebhookController extends Controller
     protected $supportName = [
         '(tat' => 'slap',
         '(ngu' => 'slap',
+        'điên' => 'slap',
         '(dam' => 'punch',
         '(boxing' => 'punch',
         '(songphi' => 'kick',
