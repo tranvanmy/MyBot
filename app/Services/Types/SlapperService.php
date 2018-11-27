edf46a45945a43ad1f8e7bd25a913514119edee8
dqhuy78 <dqhuy78@gmail.com>
2018-11-27 12:54:45 +0700

    Open slapper service

--- app/Services/Types/SlapperService.php
+++ app/Services/Types/SlapperService.php
@@ -4,23 +4,43 @@ namespace App\Services\Types;
 
 class SlapperService extends AdminService
 {
+    protected $emo = [
+        '(songphi2)',
+        '(sucvat)',
+        '(phonglon)',
+        '(xien)',
+        '(2tat)',
+        '(tat2)',
+        '(tat3)',
+        '(tat4)',
+    ];
+
     /**
      * Create response message for weather service
      */
     public function createResponse($data)
     {
         extract($data);
-        if ($fromId == env('ADMIN_CW_ID')) {
-            $targetUserId = $this->extractUserId($msg);
+        $targetUserId = $this->extractUserId($msg);
+
+        if (!$targetUserId) {
+            return '[To:' .  $fromId . ']' . PHP_EOL
+            . ' (kidding?)';
+        }
+
+        if ($targetUserId != env('ADMIN_CW_ID')) {
+            $emotionNo1 = $this->emo[array_rand($this->option)];
+            $emotionNo2 = $this->emo[array_rand($this->option)];
+            $emotionNo3 = $this->emo[array_rand($this->option)];
 
             return '[To:' . $targetUserId . ']' . PHP_EOL
-                . 'Ê ku !' . PHP_EOL
                 . '(laiday3) ' . PHP_EOL
                 . PHP_EOL
-                . '(tat2) (lengoi) (dam)';
+                . $emotionNo1 . ' ' . $emotionNo2 . ' ' . $emotionNo3;
         } else {
             return '[To:' .  $fromId . ']' . PHP_EOL
-                . ' (nonono)';
+                . ' (nonono)' . PHP_EOL
+                . ' (tat2)';
         }
     }
 }
