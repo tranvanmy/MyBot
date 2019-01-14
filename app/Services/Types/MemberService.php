f7f4c318e159121129d11cdb92aefd02ae0275bb
tranvanmy <anhmynd19101995@gmail.com>
2019-01-14 16:58:49 +0700

    add_servive_gmt

--- app/Services/Types/MemberService.php
+++ app/Services/Types/MemberService.php
@@ -10,9 +10,24 @@ class MemberService
     public function createResponse($data)
     {
         extract($data);
-
         return "[rp aid=$userId to=$roomId-$messId]\n"
-        . '[info][title]Của anh đây ạ (bow):[/title](*)Member Information: https://goo.gl/82vqF.
-(*)Member Role: https://goo.gl/3REwg3.[/info]' . PHP_EOL;
+        . '[qtmeta aid=1272369 time=1505725458][info][title]Tệp tin đã được tải lên.[/title][preview id=183426995 ht=150][download:183426995]IMG_0398.JPG (242.22 KB)[/download][/info]
+[qtmeta aid=1272369 time=1505725460][info][title]Tệp tin đã được tải lên.[/title][preview id=183426996 ht=150][download:183426996]IMG_0397.JPG (293.48 KB)[/download][/info]
+' . PHP_EOL;
+    }
+
+
+    /**
+     * Extract main content from keyword
+     *
+     * @param string $msg
+     *
+     * @return string
+     */
+    public function extractContent($msg)
+    {
+        $content = substr($msg, strpos($msg, ':') + 1);
+        $content = trim($content);
+        return $content;
     }
 }
