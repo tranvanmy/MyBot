61b6c9e252f6fcc5ba5cc4b6b5b2656df0d9b1b5
tranvanmy <anhmynd19101995@gmail.com>
2019-07-07 13:14:50 +0700

    modife_id_chatwork_and_fix_bug

--- app/Services/Types/PullService.php
+++ app/Services/Types/PullService.php
@@ -13,18 +13,18 @@ class PullService extends AdminService
         $content = $this->extractContent($msg);
 
         if (strrpos($content, 'webapp)') != 0) {
-            $to = '[To:2359460] [To:1807071] [To:2559169] [To:2559207] [To:3401286] [To:3542580] [To:3543924] [To:3525316]';
+            $to = '';
         } else {
-            $to = '[To:2359460] [To:1807071] [To:2559169] [To:2559207] [To:3401286] [To:3542580] [To:3543924] [To:3525316]';
+            $to = '[To:5085113] [To:5102249] [To:5085124]';
         }
         $pullNumber = $this->extractPullNumber($content);
 
-        ($userId == '2359460') ? $me = 'mình' : $me = 'em';
+        ($userId == '5085124') ? $me = 'mình' : $me = 'em';
 
         $reponse = "[rp aid=$userId to=$roomId-$messId]\n"
         . $to . '
 [info][title]Mọi người review giúp'.' '. $me .' '.'pull này với (bow)[/title](*) Title:' . $content . '
-(*) Link: https://github.com/framgia/group-management-tool/pull/'. $pullNumber .'[/info]'. PHP_EOL;
+(*) Link: https://bitbucket.org/vmodev/unic-cms/pull-requests/'. $pullNumber .'[/info]'. PHP_EOL;
 
         return $reponse;
     }
@@ -39,7 +39,7 @@ class PullService extends AdminService
 
     public function extractPullNumber($content)
     {
-        $number = substr($content, strlen($content) - 3);
+        $number = substr($content, strlen($content) - 2);
 
         return $number;
     }
